383e50313fb7c28e1d6e46b102c42db33d563a4d
Jacquie <Jacquelyne.grindrod@gmail.com>
2019-12-04 12:59:18 -0500

    Completed Two Fer exercise

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -7,9 +7,8 @@ package twofer
 
 // ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	var name_to_print string = "you"
+	if name != "" { name_to_print = name}
+
+	return "One for " + name_to_print + ", one for me."
 }

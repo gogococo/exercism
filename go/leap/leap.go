418f0d1ad41ae84e0fa0ab795b245d79e4985f62
Jacquie <Jacquelyne.grindrod@gmail.com>
2019-12-07 18:26:30 -0500

    Adding comments to Leap

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,15 +1,14 @@
 // This is a "stub" file.  It's a little start on your solution.
 // It's not a complete solution though; you have to write some code.
 
-// Package leap should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package leap has a function that for determining leap years.
 package leap
 
-// Given a year, identify if it is a leap year.
+// IsLeapYear determines if the given year is a leap year.
 func IsLeapYear(year int) bool {
-	if year % 4 == 0  {
-		if year % 100 == 0 {
-			if year % 400 == 0 {
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
 				return true
 			}
 			return false

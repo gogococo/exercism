e049566ef0d8aa76ea749b98187a5440da31b587
Jacquie <Jacquelyne.grindrod@gmail.com>
2019-12-08 11:55:51 -0500

    Removed redunant var

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -3,10 +3,9 @@ package twofer
 
 // ShareWith helps you to share another person.
 func ShareWith(name string) string {
-	var nameToPrint string = "you"
-	if name != "" {
-		nameToPrint = name
+	if name == "" {
+		name = "you"
 	}
 
-	return "One for " + nameToPrint + ", one for me."
+	return "One for " + name + ", one for me."
 }

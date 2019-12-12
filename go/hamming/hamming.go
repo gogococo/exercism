530182bbc9a8dc92327b1b9616413b7f0c0bfcc4
Jacquie <Jacquelyne.grindrod@gmail.com>
2019-12-12 16:17:15 -0500

    Refactoring based on mentor feedback

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -2,22 +2,20 @@ package hamming
 
 import "errors"
 
-// TODO:
-// Convert strings to rune slices first ?
-
 // Distance calculates the Hamming Distance between two Strings.
 func Distance(a, b string) (int, error) {
 	var HammingCount int = 0
+	ar := []rune(a)
+	br := []rune(b)
 
-	if len(a) == len(b) {
-		if a != b {
-			for i := 0; i < len(a); i++ {
-				if a[i] != b[i] {
-					HammingCount++
-				}
-			}
-		}
-		return HammingCount, nil
+	if len(ar) != len(br) {
+		return -0, errors.New("strings are not same length")
 	}
-	return -0, errors.New("strings are not same length")
+
+	for i := 0; i < len(ar); i++ {
+		if ar[i] != br[i] {
+			HammingCount++
+		}
+	}
+	return HammingCount, nil
 }
